c6e7c867582222ab2990f7cdf011e37188621eea
keandrake-24 <keandre.imposter@gmail.com>
2026-02-15 13:14:14 +0700

    added install script and swayidle fix

---
+++ install.sh
@@ -0,0 +1,6 @@
+ln -s  */  ~/.config/
+sudo cp -rv wallpapers/ /usr/share/backgrounds
+sudo dnf copr enable wef/gtklock
+sudo dnf copr enable elxreno/jetbrains-mono-fonts -y
+sudo dnf install waybar rofi alacritty dunst lxappearance network-manager-applet gtklock thunar swayidle brightnessctl pulsemixer arc-theme papirus-icon-theme jetbrains-mono-fonts fontawesome-fonts-all xdg-desktop-portal xdg-desktop-portal-wlr
+
